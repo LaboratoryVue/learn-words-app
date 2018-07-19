61d085261e812085f451678aea4c1c5cbd6cf9e0
gearmobile <gearmobile@gmail.com>
2018-07-19 19:26:03 +0300

    fix

--- src/views/WordsTraining.vue
+++ src/views/WordsTraining.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="training">
-    <h3 class="text-capitalize mb-4">select translation</h3>
-    <p class="word">{{ current.origin }}</p>
-    <ul class="list-group">
-      <li class="list-group-item mb-2" v-for="(translate,i) in translated" :key="i">
+  <div class="training word">
+    <h3 class="text-capitalize mb-4 training__title">select translation</h3>
+    <p class="word__current">{{ current.origin }}</p>
+    <ul class="list-group word__list">
+      <li @click="onSelect(i)" class="list-group-item mb-2 word__item" v-for="(translate,i) in translated" :key="i">
         {{ translate }}
       </li>
     </ul>
@@ -23,12 +23,14 @@ export default {
     translated() {
       const result = [];
       for (let index = 0; index < this.words.length; index++) {
-        // if (this.words[index].translated !== this.current.translated) {
-        //   result.push(this.words[index].translated);
-        // }
         result.push(this.words[index].translated);
       }
       return result.slice(0, 6);
+    }
+  },
+  methods: {
+    onSelect(index) {
+      console.log(`${index}`);
     }
   }
 };
@@ -36,9 +38,20 @@ export default {
 
 <style lang="scss" scoped>
 .word {
-  text-align: center;
-  color: blue;
-  font-weight: 700;
-  font-size: 2rem;
+  //
+  &__current {
+    text-align: center;
+    color: blue;
+    font-weight: 700;
+    font-size: 2rem;
+  }
+  &__item {
+    cursor: pointer;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: lightblue;
+    }
+  }
 }
 </style>
